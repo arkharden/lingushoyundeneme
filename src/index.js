56d6d57f697af4cd0fe9df378e1f30d36b5086f1
Halil Yıldırım <65043739+arkharden@users.noreply.github.com>
2022-07-19 01:46:45 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,86 @@
+/* <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="../src/app.css" />
+  </head>
+  <body>
+    <div class="container">
+      <header>
+        <h1>Köstebeğe Vur</h1>
+        <h3>Skor: <span id="skor">0</span></h3>
+        <button id="baslat">Baslat</button>
+      </header>
+      <h5 id="süre">15</h5>
+      <div class="game-container">
+        <div class="toprak">
+          <div class="köstebek"></div>
+        </div>
+        <div class="toprak">
+          <div class="köstebek"></div>
+        </div>
+        <div class="toprak">
+          <div class="köstebek"></div>
+        </div>
+        <div class="toprak">
+          <div class="köstebek"></div>
+        </div>
+        <div class="toprak">
+          <div class="köstebek"></div>
+        </div>
+        <div class="toprak">
+          <div class="köstebek"></div>
+        </div>
+      </div>
+    </div>
+    <script src="../src/app.js"></script>
+  </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+import  '../app.css';
+return (
+    <Grid container>
+      <header>
+        Köstebeğe Vur
+        <h3>Skor: <span id="skor">0</span></h3>
+        <button id="baslat">Baslat</button>
+      </header>
+
+      <h5 id="süre">15</h5>
+      <Grid class="game-container">
+        <Grid class="toprak">
+          <Grid class="köstebek"></Grid>
+        </Grid>
+        <Grid class="toprak">
+          <Grid class="köstebek"></Grid>
+        </Grid>
+        <Grid class="toprak">
+          <Grid class="köstebek"></Grid>
+        </Grid>
+        <Grid class="toprak">
+          <Grid class="köstebek"></Grid>
+        </Grid>
+        <Grid class="toprak">
+          <Grid class="köstebek"></Grid>
+        </Grid>
+        <Grid class="toprak">
+          <Grid class="köstebek"></Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+    
+    )
+*/
